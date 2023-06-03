2bd5a780c04a45736a9a6a663d6bcc62ea0f2504
pitzzahh <pitzzahh@araopj.tech>
2023-06-03 21:47:25 +0800

    chore: simplified getting movies from api

--- CineWave/MVVM/ViewModel/HomeViewModel.cs
+++ CineWave/MVVM/ViewModel/HomeViewModel.cs
@@ -26,22 +26,17 @@ public class HomeViewModel : Core.ViewModel
     public void GetMoviesFromApi()
     {
         // Fetch movie data from TMDb API
-        var movieData = TmdbHelper.GetTopMovies();
-
         // Parse the JSON response
-        var jsonObject = JObject.Parse(movieData);
-        var results = jsonObject["results"];
+        var results = JObject.Parse(TmdbHelper.GetTopMovies())["results"];
 
         if (results == null) return;
         foreach (var result in results)
         {
-            var title = result["original_title"]?.ToString();
-            var overview = result["overview"]?.ToString();
-            var posterPath = result["poster_path"]?.ToString();
-            var imageUrl = $"https://image.tmdb.org/t/p/w500/{posterPath}";
-
-            _movies.Add(new MovieCardViewModel(imageUrl, title, overview));
+            _movies.Add(new MovieCardViewModel(
+                $"https://image.tmdb.org/t/p/w500/{result["poster_path"]}",
+                result["original_title"]?.ToString(),
+                result["overview"]?.ToString()
+            ));
         }
-
     }
 }
